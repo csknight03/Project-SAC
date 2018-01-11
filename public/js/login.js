fcca9345dc5f03755e769fb73e74b0c5d74d6939
alexwalz <alexwalz@icloud.com>
2018-01-11 15:30:41 -0700

    animations

--- public/js/login.js
+++ public/js/login.js
@@ -27,18 +27,7 @@ $(window).scroll(function () {
         $(".fourth-image").fadeIn("slow")
     }
 
-    if ($(this).scrollTop() > 1150) {
-        $(".chore-quote-1").fadeOut("slow")
-    }
 
-
-    if ($(this).scrollTop() < 1100) {
-        $(".chore-quote-1").fadeIn("slow")
-    }
-
-    if ($(this).scrollTop() < 1100) {
-        $(".chore-quote-2").fadeIn("slow")
-    }
 
 
 });
