b6003241428849b7af23c8f116620e482a719983
alexwalz <alexwalz@icloud.com>
2018-01-11 16:59:16 -0700

    hopeful heroku fix?

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,7 +9,7 @@ var path = require("path");
 module.exports = function(app) {
   
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/families", function(req, res) {
